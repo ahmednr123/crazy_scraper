a716d6acf6a0bd14a51f27e0591b55853a4b2c2b
Ahmed Noor <ahmednr123@gmail.com>
2019-01-25 14:52:48 +0530

    changes

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/*const fs = require('fs');
+const fs = require('fs');
 
 let arr = new Array(20);
 let buff = Buffer.from(arr);
@@ -7,12 +7,4 @@ fs.open('./testfile', 'a+', (err, fd) => {
 	fs.read(fd, buff, 0, 5, 18, (err, bytes, buffer) => {
 		console.log(buffer.toString('utf8'));
 	})
-})*/
-
-function fun () {
-	this.martin = 'Hallo';
-}
-
-let bu = new fun();
-
-console.log(bu.martin)
+})
